898470a955b89ebef5c0765e1c1f0a0f97e4345c
kubudubu <maciejewski.kuba@gmail.com>
2018-08-29 22:57:29 +0200

    minor routing update

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
     resources :trips, only: :create
 
     namespace :stats do
-      get :weekly, to: 'stats#weekly'
-      get :monthly, to: 'stats#monthly'
+      get :weekly, to: "stats#weekly"
+      get :monthly, to: "stats#monthly"
     end
   end
 
-  match '*unknown_route', :to => 'application#handle_routing_error', :via => :all
+  match "*unknown_route", :to => "application#handle_routing_error", :via => :all
 end
